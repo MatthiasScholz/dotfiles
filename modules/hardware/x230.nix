ffc738124a4efda1bfc3e475f822dd86c5b8be68
Matthias Scholz <MatthiasScholz@users.noreply.github.com>
2023-03-17 09:21:42 +0100

    :construction: additional hardware support for x230

---
+++ modules/hardware/x230.nix
@@ -0,0 +1,16 @@
+{
+  description = "Additional hardware support for Thinkpad x230";
+  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+  outputs = { self, nixpkgs, nixos-hardware }: {
+    # replace <your-hostname> with your actual hostname
+    nixosConfigurations.<your-hostname> = nixpkgs.lib.nixosSystem {
+      # ...
+      modules = [
+        # ...
+        # add your model from this list: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
+        nixos-hardware.nixosModules.lenovo-thinkpad-x230
+      ];
+    };
+  };
+}
