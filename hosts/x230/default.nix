6a5ef43a1aec40396a3cec765367b1fec94edaf3
Matthias Scholz <MatthiasScholz@users.noreply.github.com>
2023-03-18 18:10:42 +0100

    :sparkles: filesystem configuration for x230

--- hosts/x230/default.nix
+++ hosts/x230/default.nix
@@ -5,6 +5,15 @@
     ./hardware-configuration.nix
   ];
 
+  # filesystem configuration updates based on BTRFS
+  fileSystems = { 
+     "/".options = [ "compress=zstd" ]; 
+     "/home".options = [ "compress=zstd" ]; 
+     "/nix".options = [ "compress=zstd" "noatime" ]; 
+     "/swap".options = [ "noatime" ]; 
+  }; 
+  swapDevices = [ { device = "/swap/swapfile"; } ];
+
   # Select internationalisation properties.
   #i18n = {
   #  defaultLocale = "en_US.UTF-8";
