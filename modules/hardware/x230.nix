c953a5c2be8c2f934e88cf525e3b5f8f2f2c9052
Matthias Scholz <MatthiasScholz@users.noreply.github.com>
2023-03-18 13:36:12 +0100

    :construction: :poop: deactivate specific hardware configuration

--- modules/hardware/x230.nix
+++ modules/hardware/x230.nix
@@ -1,16 +1,16 @@
-{
-  #description = "Additional hardware support for Thinkpad x230";
-  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+#{
+#  #description = "Additional hardware support for Thinkpad x230";
+#  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-  outputs = { self, nixpkgs, nixos-hardware }: {
-    # replace <your-hostname> with your actual hostname
-    nixosConfigurations.x230 = nixpkgs.lib.nixosSystem {
-      # ...
-      modules = [
-        # ...
-        # add your model from this list: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
-        nixos-hardware.nixosModules.lenovo-thinkpad-x230
-      ];
-    };
-  };
-}
+#  outputs = { self, nixpkgs, nixos-hardware }: {
+#    # replace <your-hostname> with your actual hostname
+#    nixosConfigurations.x230 = nixpkgs.lib.nixosSystem {
+#      # ...
+#      modules = [
+#        # ...
+#        # add your model from this list: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
+#        nixos-hardware.nixosModules.lenovo-thinkpad-x230
+#      ];
+#    };
+#  };
+#}
