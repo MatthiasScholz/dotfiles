8ae711853fee4fbac7c3b844ade44d499fb4322d
Matthias Scholz <MatthiasScholz@users.noreply.github.com>
2023-03-19 20:59:11 +0100

    :construction: streamlining configuration

--- hosts/x230/default.nix
+++ hosts/x230/default.nix
@@ -64,18 +64,13 @@
         default = "brave";
         brave.enable = true;
         #firefox.enable = true;
-        #qutebrowser.enable = true;
       };
-      gaming = {
-        # steam.enable = true;
-        # emulators.enable = true;
-        # emulators.psx.enable = true;
-      };
+      gaming = {};
       media = {
         daw.enable = true;
         documents.enable = true;
         graphics.enable = true;
-        #mpv.enable = true;
+        mpv.enable = true;
         #recording.enable = true;
         spotify.enable = true;
       };
@@ -122,6 +117,7 @@
   services.openssh.startWhenNeeded = true;
 
   networking.networkmanager.enable = true;
+  networking.wireless.enable = true;
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
@@ -129,46 +125,7 @@
   networking.interfaces.wlp3sO.useDHCP = lib.mkDefault true;
   networking.interfaces.enp0s25.useDHCP = lib.mkDefault true;
 
-  ## Personal backups
-  # Syncthing is a bit heavy handed for my needs, so rsync to my NAS instead.
-  #systemd = {
-    #services.backups = {
-    #  description = "Backup /usr/store to NAS";
-    #  wants = [ "usr-drive.mount" ];
-    #  path  = [ pkgs.rsync ];
-    #  environment = {
-    #    SRC_DIR  = "/usr/store";
-    #    DEST_DIR = "/usr/drive";
-    #  };
-    #  script = ''
-    #    rcp() {
-    #      if [[ -d "$1" && -d "$2" ]]; then
-    #        echo "---- BACKUPING UP $1 TO $2 ----"
-    #        rsync -rlptPJ --chmod=go= --delete --delete-after \
-    #            --exclude=lost+found/ \
-    #            --exclude=@eaDir/ \
-    #            --include=.git/ \
-    #            --filter=':- .gitignore' \
-    #            --filter=':- $XDG_CONFIG_HOME/git/ignore' \
-    #            "$1" "$2"
-    #      fi
-    #    }
-    #    rcp "$HOME/projects/" "$DEST_DIR/projects"
-    #    rcp "$SRC_DIR/" "$DEST_DIR"
-    #  '';
-    #  serviceConfig = {
-    #    Type = "oneshot";
-    #    Nice = 19;
-    #    IOSchedulingClass = "idle";
-    #    User = config.user.name;
-    #    Group = config.user.group;
-    #  };
-    #};
-    #timers.backups = {
-    #  wantedBy = [ "timers.target" ];
-    #  partOf = [ "backups.service" ];
-    #  timerConfig.OnCalendar = "*-*-* 00,12:00:00";
-    #  timerConfig.Persistent = true;
-    #};
-  #};
+  hardware.opengl.enable = true;
+  
+  time.timeZon = "Europe/Berlin";
 }
