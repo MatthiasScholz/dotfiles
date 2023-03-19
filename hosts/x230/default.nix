50397483a341056576d5c2752c181930317097ba
Matthias Scholz <MatthiasScholz@users.noreply.github.com>
2023-03-19 21:46:51 +0100

    :construction: disable due to error

--- hosts/x230/default.nix
+++ hosts/x230/default.nix
@@ -118,7 +118,8 @@
   services.openssh.startWhenNeeded = true;
 
   networking.networkmanager.enable = true;
-  networking.wireless.enable = true;
+  # FIXME not working
+  # networking.wireless.enable = true;
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
