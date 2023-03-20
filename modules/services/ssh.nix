d816a59cca633f7a937d4d68ff47ed9f3c16e83a
Matthias Scholz <matthias.scholz@thoughtworks.com>
2023-03-20 21:43:58 +0100

    :bug: rename warning

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -11,10 +11,11 @@ in {
   config = mkIf cfg.enable {
     services.openssh = {
       enable = true;
-      kbdInteractiveAuthentication = false;
-      passwordAuthentication = false;
+      settings.KbdInteractiveAuthentication = false;
+      settings.PasswordAuthentication = false;
     };
 
+    # TODO provide own user configuration
     user.openssh.authorizedKeys.keys =
       if config.user.name == "hlissner"
       then [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB71rSnjuC06Qq3NLXQJwSz7jazoB+umydddrxL6vg1a hlissner" ]
