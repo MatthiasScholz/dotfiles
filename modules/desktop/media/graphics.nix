dd5ac45f87b4d76d37269e3fecafe0789d2eb56c
Matthias Scholz <matthias.scholz@thoughtworks.com>
2023-03-20 22:46:31 +0100

    :bug: broken package usage

--- modules/desktop/media/graphics.nix
+++ modules/desktop/media/graphics.nix
@@ -37,7 +37,8 @@ in {
       (if cfg.raster.enable then [
         krita
         gimp
-        gimpPlugins.resynthesizer  # content-aware scaling in gimp
+        # FIXME marked as broken
+        # gimpPlugins.resynthesizer  # content-aware scaling in gimp
       ] else []) ++
 
       # Sprite sheets & animation
