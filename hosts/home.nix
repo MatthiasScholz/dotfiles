4b34023714d9d3f09baf5802a57667a70294e2b5
Matthias Scholz <MatthiasScholz@users.noreply.github.com>
2023-03-20 20:33:10 +0100

    :construction: :poop: unused option

--- hosts/home.nix
+++ hosts/home.nix
@@ -30,10 +30,12 @@ in {
   i18n.defaultLocale = mkDefault "en_US.UTF-8";
   # For redshift, mainly
   location = (if config.time.timeZone == "America/Toronto" then {
-    latitude = 43.70011;
+    # FIXME not used
+    # latitude = 43.70011;
     longitude = -79.4163;
   } else if config.time.timeZone == "Europe/Copenhagen" then {
-    latitude = 55.88;
+    # FIXME not used
+    # latitude = 55.88;
     longitude = 12.5;
   } else {});
 
