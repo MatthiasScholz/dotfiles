7f4f6aba3f0ef02ca46ce48f676ba4052836940e
Matthias Scholz <matthias.scholz@thoughtworks.com>
2023-03-21 20:58:17 +0100

    :sparkles: :construction: thunar file browser support

--- modules/desktop/media/documents.nix
+++ modules/desktop/media/documents.nix
@@ -10,12 +10,16 @@ in {
     enable = mkBoolOpt false;
     pdf.enable = mkBoolOpt false;
     ebook.enable = mkBoolOpt false;
+    files.enable = mkBoolOpt true;
   };
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
       (mkIf cfg.ebook.enable calibre)
       (mkIf cfg.pdf.enable   evince)
+      (mkIf cfg.files.enable xfce.thunar)
+      (mkIf cfg.files.enable xfce.thunar-volman)
+      (mkIf cfg.files.enable xfce.thunar-archive-plugin)
       # zathura
     ];
 
