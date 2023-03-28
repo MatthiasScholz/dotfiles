29970057bd2649067f1e8d2f223ae4842983ad69
Matthias Scholz <matthias.scholz@thoughtworks.com>
2023-03-28 22:05:55 +0200

    :sparkles: iac module including aws

---
+++ modules/dev/iac.nix
@@ -0,0 +1,35 @@
+# modules/dev/iac.nix ---
+
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let devCfg = config.modules.dev;
+    cfg = devCfg.iac;
+in {
+  options.modules.dev.iac = {
+    enable = mkBoolOpt false;
+    xdg.enable = mkBoolOpt devCfg.xdg.enable;
+    aws = rec {
+      enable = mkBoolOpt false;
+    };
+  };
+
+  config = mkMerge [
+    (mkIf cfg.enable {
+      user.packages = with pkgs; [
+        terraform
+        # TODO my.terramate
+        tflint
+        tfsec
+        conftest
+        open-policy-agent
+        (mkif (cfg.aws.enable)
+          awscli2
+          aws-vault
+          # TODO my.clok-sm
+        )
+      ];
+    })
+  ];
+}
