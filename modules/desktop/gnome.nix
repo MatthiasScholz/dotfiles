9b7c9951058490941effd6db3e257e232c1e1068
Matthias Scholz <matthias.scholz@thoughtworks.com>
2023-03-29 20:41:42 +0200

    :sparkles: tailoring gnome3

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -21,6 +21,34 @@ in {
       };
     };
 
+    # SEE: https://nixos.wiki/wiki/GNOME
+    environment.systemPackages = with pkgs; [
+      gnomeExtensions.appindicator
+    ];
+
+    services.udev.packages = with pkgs; [
+      gnome.gnome-settings-daemon
+    ];
+
+    environment.gnome.excludePackages = (with pkgs; [
+      gnome-photos
+      gnome-tour
+      ]) ++ (with pkgs.gnome; [
+        cheese # webcam tool
+        gnome-music
+        gnome-terminal
+        gedit # text editor
+        epiphany # web browser
+        geary # email reader
+        #evince # document viewer
+        #gnome-characters
+        totem # video player
+        tali # poker game
+        iagno # go game
+        hitori # sudoku game
+        atomix # puzzle game
+      ]);
+
     # link recursively so other modules can link files in their folders
     #home.configFile = {
     #  "gnome" = {
